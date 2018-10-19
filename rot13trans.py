2c53432ea157ee3131cc081754e6b17b70f3e110
Stephen Homick <shomick@datto.com>
2018-10-19 16:35:20 -0400

    Added arg parsing. Need tweaking

--- rot13trans.py
+++ rot13trans.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 
+import argparse
 import string
+import sys
+#
+# Set up CLI argument parsing.
+#
+parser = argparse.ArgumentParser()
+parser.add_argument('--asciitorot', help='Convert plain text to ROT13 cipher.', action='store_true')
+parser.add_argument('--rottoascii', help='Convert ROT13 to plain text.', action='store_true')
+args = parser.parse_args()
 
 #ROT13 intab and outtab representing the conversion
 intab = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz"
 outtab = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
 
-str = input("String:> ")
-trantab = str.maketrans(intab,outtab)
-print(str.translate(trantab))
+try:
+    if args.asciitorot:
+        str = args.asciitorot
+        trantab = str.maketrans(intab,outtab)
+        print(str.translate(trantab))
+    elif args.rottoascii:
+        str = input("[ROT13 --> ASCII selected]--String:> ")
+        trantab = str.maketrans(outtab,intab)
+        print(str.translate(trantab))
+    else:
+        sys.exit()
+except KeyboardInterrupt:
+    print("User exited voluntarily")
