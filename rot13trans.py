e49b078fd12228782cc48b6ced351723e1aaa9e1
Stephen-T-Homick <shomick87@gmail.com>
2018-10-20 19:30:04 -0400

    Format string added in usage function.

--- rot13trans.py
+++ rot13trans.py
@@ -9,12 +9,19 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('--asciitorot', help='Convert plain text to ROT13 cipher.', action='store_true')
 parser.add_argument('--rottoascii', help='Convert ROT13 to plain text.', action='store_true')
+#parser.add_argument("-h", "--help", action='help', help="Show this help message")
 args = parser.parse_args()
 
 #ROT13 intab and outtab representing the conversion
 intab = "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz"
 outtab = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
 
+def usage():
+    print(('usage: rot13trans.py [-h] [--asciitorot] [--rottoascii]'
+    'optional arguments:'
+    '-h, --help    show this help message and exit'
+   '--asciitorot  Convert plain text to ROT13 cipher.'
+    '--rottoascii  Convert ROT13 to plain text.)').format(3,5))
 try:
     if args.asciitorot:
         str = args.asciitorot
@@ -25,6 +32,7 @@ try:
         trantab = str.maketrans(outtab,intab)
         print(str.translate(trantab))
     else:
-        sys.exit()
+        print("No arguments supplied")
+        usage()
 except KeyboardInterrupt:
     print("User exited voluntarily")
