412e5e4d13a5f2da8c99ca1d290ecc90439a5a15
Jason <jdale215@gmail.com>
2019-02-11 11:00:03 -0500

    basic str to ascii conversion

--- rot13trans.py
+++ rot13trans.py
@@ -12,8 +12,9 @@ import sys
 ### Set up CLI argument parsing.
 ###
 parser = argparse.ArgumentParser()
-parser.add_argument('--asciitorot', help='Convert plain text to ROT13 cipher.', action='store_true')
-parser.add_argument('--rottoascii', help='Convert ROT13 to plain text.', action='store_true')
+parser.add_argument('-a', '--asciitorot', help='Convert plain text to ROT13 cipher.', action='store_true')
+parser.add_argument('-r', '--rottoascii', help='Convert ROT13 to plain text.', action='store_true')
+parser.add_argument('-n', '--rotnumber', help='Convert ROT13 to plain text.', action='store_true', default='13')
 args = parser.parse_args()
 
 #ROT13 intab and outtab representing the conversion
@@ -22,12 +23,13 @@ outtab = "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
 
 def usage():
     print("""
-    usage: rot13trans.py [-h] [--asciitorot] [--rottoascii]
+    usage: rot13trans.py [-h] [-a, --asciitorot] [-r --rottoascii]
 
 optional arguments:
   -h, --help    show this help message and exit
-  --asciitorot  Convert plain text to ROT13 cipher.
-  --rottoascii  Convert ROT13 to plain text.
+  -a, --asciitorot  Convert plain text to ROT13 cipher.
+  -r, --rottoascii  Convert ROT13 to plain text.
+  -n, --rotnumber  Pick a ROT number to translate too/from
   """)
 
 while True:
@@ -40,6 +42,15 @@ while True:
             strput = input("[ASCII --> ROT 13 Selected]--String:> ")
             trantab = strput.maketrans(intab,outtab)
             print(strput.translate(trantab))
+            output = []
+            for c in strput:
+                i = ord(c)
+                i = int(i) + int(args.rotnumber)
+                o = (chr(i))
+                output.append(o)
+            output = ''.join(output)
+            # TODO: Figure out the math to make sure the add/subtract doesn't go outside the ascii codes for a-z and A-Z (97-122 and 65-90)
+            print(output)
         elif args.rottoascii:
             strput = input("[ROT13 --> ASCII selected]--String:> ")
             trantab = strput.maketrans(outtab,intab)
